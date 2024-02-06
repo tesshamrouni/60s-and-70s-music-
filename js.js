779771bd64b9b782d36b6e3c638b060d3d8b19cd
tesshamrouni <tessnimhamrouni18@gmail.com>
2024-02-06 20:43:34 +0100

    Add to remote GitHub

---
+++ js.js
@@ -0,0 +1,32 @@
+function demo(){
+    document.getElementById('intro').style.color="grey";
+}
+
+function demo1(){
+    document.getElementById("load").style.color="white";
+}
+
+
+function draft(){
+    document.getElementById('box1').style.backgroundColor="#aa7713";
+}
+
+
+function draft1(){
+    document.getElementById('box2').style.backgroundColor="#aa7713";
+}
+
+
+function finale(){
+    alert( "The Form Was Submitted");
+}
+
+
+function boum(){
+    document.getElementById('data7').innerHTML="YOU ARE NOT "
+}
+
+
+function color(){
+    document.getElementById('box5').style.backgroundColor="#aa7713"
+}
